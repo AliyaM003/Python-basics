890472ec53e8c22e25113858234dd3729945beb9
AliyaM003 <167082547+AliyaM003@users.noreply.github.com>
2024-04-18 21:22:57 +0530

    Create prgm23.py

---
+++ prgm23.py
@@ -0,0 +1,14 @@
+def solve it 2():
+print(:this is line 123")
+def solve it 4():
+print("i am solve it 4")
+print('this is getting executed")
+      solve it 2()
+      def solve it()
+      def solve it():
+      print("this is line 111")
+      print("this is line 112")
+      solve it 4()
+      def sumoftwonumbers(num1,num2):
+      solve it()
+      

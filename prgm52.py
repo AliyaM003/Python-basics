6cc183dca69703a32256e8bc2f6b9abe2b02abd5
AliyaM003 <167082547+AliyaM003@users.noreply.github.com>
2024-04-27 19:57:28 +0530

    Create prgm52.py

---
+++ prgm52.py
@@ -0,0 +1,16 @@
+import math
+import os
+import random
+import re
+import sys
+# remainder=0->even number
+# remainder!=0->odd number
+n = int(input().strip())
+if n%2!=0:
+  print("wierd")
+else:
+  if n>=2 and n<=5:
+    print("not wierd")
+  elif n>=6 and n<=20 
+    print("wierd")
+  

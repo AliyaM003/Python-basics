de043f1e863e7abd9271d6e9dc9a8a5d49cd18c3
AliyaM003 <167082547+AliyaM003@users.noreply.github.com>
2024-04-27 19:34:03 +0530

    Create prgm48.py

---
+++ prgm48.py
@@ -0,0 +1,54 @@
+def storeBFS(startNode, result, adj, n, visited):
+    visited[startNode] = True
+    Q = [startNode]
+ 
+    while len(Q) > 0:
+        currNode = Q.pop(0)
+        result.append(currNode)
+ 
+        for neighbour in adj[currNode]:
+            if visited[neighbour] == False:
+                visited[neighbour] = True
+                Q.append(neighbour)
+ 
+
+
+def printBFSTraversal(adj, n):
+    visited = [False] * n 
+    # n = 5 
+    # [False, False, False, False, False]
+    result = []
+ 
+    # to handle different components
+    for i in range(n):
+        if visited[i] == False:
+            storeBFS(i, result, adj, n, visited)
+ 
+    print("BFS is:", result)
+ 
+# adjacency list construction
+ 
+n, m = map(int, input().split())
+adj = []
+for i in range(n): 
+    adj.append([])
+ 
+for i in range(m):
+    u, v = map(int, input().split())
+    adj[u].append(v)
+    adj[v].append(u)
+ 
+printBFSTraversal(adj, n)
+
+#//INPUT
+#6 7
+#0 1
+#0 2
+#0 3
+#1 0
+#1 5
+#2 5
+#2 3
+#//OUTPUT
+#BFS is: [0, 1, 2, 3, 5, 4]
+

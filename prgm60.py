1391706030208459751b0b80f2cb89b4ce277c42
AliyaM003 <167082547+AliyaM003@users.noreply.github.com>
2024-04-27 20:25:08 +0530

    Create prgm60.py

---
+++ prgm60.py
@@ -0,0 +1,17 @@
+n = int(input())
+spaces = n - 1
+for row in range(1, n + 1):
+      
+   for space in range(spaces):
+       print(" ", end = "")
+   spaces -= 1
+  
+   for col in range(1, row + 1):
+       print(col, end = "")
+   print()
+
+
+#1
+#12
+#123
+#1234

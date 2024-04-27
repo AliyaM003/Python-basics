444b23f981ec50637629f4a1184e4a9fe510ec47
AliyaM003 <167082547+AliyaM003@users.noreply.github.com>
2024-04-27 19:44:40 +0530

    Create prgm51.py

---
+++ prgm51.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+#integer/floor division -> integer part
+#float division -> integer part along with decimal
+print(a/b)
+print(a/b)
+
+#output:
+#a =3
+#b = 5
+#result of integer=3/5=0
+#result of float = 3/5=6.000
